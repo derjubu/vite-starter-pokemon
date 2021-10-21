a12b4521f3ea4e1dfdd4141b864d17252df3393f
Jeremias Erbs <jerry@badabam.de>
2021-10-21 15:42:57 +0200

    Refactor dom function names

--- src/lib/dom.js
+++ src/lib/dom.js
@@ -1,19 +1,3 @@
-export function getElement(selector, target = document) {
-  return target.querySelector(selector)
-}
-
-export function getAllElements(selector, target = document) {
-  return target.querySelectorAll(selector)
-}
-
-export function getDataJs(dataJs, target = document) {
-  return target.querySelector('[data-js=' + dataJs + ']')
-}
-
-export function getAllDataJs(dataJs, target = document) {
-  return target.querySelectorAll('[data-js=' + dataJs + ']')
-}
-
 export function createElement(elementType, props, ...children) {
   const element = document.createElement(elementType)
   if (props) {
@@ -23,4 +7,20 @@ export function createElement(elementType, props, ...children) {
     element.append(...children)
   }
   return element
+}
+
+export function getByDataJs(dataJs, target = document) {
+  return target.querySelector('[data-js=' + dataJs + ']')
+}
+
+export function getAllByDataJs(dataJs, target = document) {
+  return target.querySelectorAll('[data-js=' + dataJs + ']')
+}
+
+export function getBySelector(selector, target = document) {
+  return target.querySelector(selector)
+}
+
+export function getAllBySelector(selector, target = document) {
+  return target.querySelectorAll(selector)
 }
