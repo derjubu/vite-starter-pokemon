e7edcb04729bebc9bfab3a18e9fe8430532a8652
Julian Buchholz <github@julianbuchholz.com>
2021-10-23 12:19:26 +0200

    Add types to pokemon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,13 @@
 import { createElement } from '../lib/dom.js'
 import './Card.css'
 
-export default function Card({ name, id, image }) {
+export default function Card({ image, name, id, type }) {
   const el = createElement('section', {
     className: 'Card',
-    innerHTML: `<img src="${image}" alt="${name}" width="96" height="96">
-    This is ${name}, it is the Pokemon with the ID ${id}. It has the types `,
+    innerHTML: `<li class="card"><img src="${image}" alt="${name}" width="96" height="96">
+    This is ${name}, it is the Pokemon with the ID ${id}. It has the types ${[
+      type,
+    ]}</li>`,
   })
 
   return el
